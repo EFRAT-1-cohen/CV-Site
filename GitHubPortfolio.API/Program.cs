5397a7d93816d18689bc25701ce0c13b1176758b
EFRAT-1-cohen <efratco054@gmail.com>
2025-12-07 00:04:41 +0200

    1

--- GitHubPortfolio.API/Program.cs
+++ GitHubPortfolio.API/Program.cs
@@ -35,9 +35,7 @@ builder.Services.AddCors(options =>
               .AllowAnyHeader();
     });
 });
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline
 app.UseSwagger();
 app.UseSwaggerUI();
